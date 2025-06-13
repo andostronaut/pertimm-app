bdc989b12b26e3555b77c5b4539fbf76d4a90d59
Ando <hi@andostronaut.com>
2025-06-13 15:34:30 +0300

    fix: remove unused script

--- scripts/config.ts
+++
@@ -1,9 +0,0 @@
-export const BASE_URL = 'https://hire-game.pertimm.dev';
-export const LOGIN_ENDPOINT = `${BASE_URL}/login`;
-export const CREATE_APPLICATION_ENDPOINT = `${BASE_URL}/api/v1.1/job-application-request/`;
-
-export const EMAIL = 'your.email@example.com';
-export const FIRST_NAME = 'YourFirstName';
-export const LAST_NAME = 'YourLastName';
-export const USERNAME = 'your-username';
-export const PASSWORD = 'your-password';
